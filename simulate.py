07a1025c1149e2a1b199bd95a6dceb7e8d83183a
Bruce W. Lowther <bwlowther@micron.com>
2024-09-13 16:29:31 -0400

    Format and remove unused packages.

--- simulate.py
+++ simulate.py
@@ -16,8 +16,7 @@ from pathlib import Path
 from datetime import datetime
 
 from pydantic import BaseModel, Field
-from datetime import timedelta
-from itertools import groupby, product, chain, repeat, count
+from itertools import product, chain, count
 from functools import cached_property
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -41,12 +40,15 @@ class EmployeeConfig(BaseModel):
     def generate_employee_config(cls, max_employees) -> List['EmployeeConfig']:
         """Generate the parameters for an EmployeeConfig. number of places is a little strange from the docs: For example, product(A, repeat=4) means the same as product(A, A, A, A).  So in this case, build a tuple of three items."""
         num_places = 3
-        theaters = [cls(num_cashiers=config[0], num_servers=config[1], num_ushers=config[2]) for config in product(range(1, max_employees + 1), repeat=num_places) if sum(config) <= max_employees]
+        theaters = [cls(num_cashiers=config[0], num_servers=config[1], num_ushers=config[2]) for config in
+                    product(range(1, max_employees + 1), repeat=num_places) if sum(config) <= max_employees]
         theaters.sort(key=lambda e: e.total_employees)
         return theaters
 
+
 class Theater_Metric(BaseModel):
-    wait_times: List[float] = Field(default_factory=list, description="the time consumed between arrival and entering in to the theater.")
+    wait_times: List[float] = Field(default_factory=list,
+                                    description="the time consumed between arrival and entering in to the theater.")
     usher_queue: List[int] = Field(default_factory=list)
     cashier_queue: List[int] = Field(default_factory=list)
     server_queue: List[int] = Field(default_factory=list)
@@ -57,8 +59,6 @@ class Theater_Metric(BaseModel):
         return statistics.mean(self.wait_times)
 
 
-
-
 class Theater:
     DEFAULT_PRELOAD_CUSTOMERS = 3
     DEFAULT_CASHIER_BALK_LIMIT = 15
@@ -66,7 +66,9 @@ class Theater:
     Cashier: Number of people that can provide a ticket
     Server: Number of people that can vend food.
     Usher: Number of people that can check a ticket and admit them to the movie."""
-    def __init__(self, employee_config: EmployeeConfig, env = None, cashier_balk_limit: Optional[int] = None, preload_customer_count: Optional[int] = None):
+
+    def __init__(self, employee_config: EmployeeConfig, env=None, cashier_balk_limit: Optional[int] = None,
+                 preload_customer_count: Optional[int] = None):
         # Pysim execution environment
         self.employee_config = employee_config
         # a customer will walk away if there are too many people in line for cashier
@@ -107,7 +109,7 @@ class Theater:
     def avg_wait_time(self):
         return statistics.mean(self.metric.wait_times)
 
-    def get_result(self) -> Dict[str,Any]:
+    def get_result(self) -> Dict[str, Any]:
         """return a dictionary that contains all the data that might be interesting in a data frame."""
         return dict(chain(self.employee_config.model_dump().items(),
                           self.metric.model_dump().items()))
@@ -134,7 +136,7 @@ class Theater:
 
             # Moviegoer heads into the theater
             self.metric.customer_count = self.metric.customer_count + 1
-            #capture the time it took them to go from arrival to entering the theater.
+            # capture the time it took them to go from arrival to entering the theater.
             self.metric.wait_times.append(self.env.now - arrival_time)
             # Capture the cashier queue, usher queue, and server queue when a moviegoer enters the theater.
             self.metric.cashier_queue.append(len(self.cashier.queue))
@@ -152,7 +154,6 @@ class Theater:
                 yield self.env.timeout(0.20)  # Wait a bit before generating new moviegoer
             self.env.process(self.go_to_movies(moviegoer))
 
-
     def run(self):
         self.env.process(self.incoming_moviegoers_gen())
         self.env.run(until=90)
@@ -160,12 +161,11 @@ class Theater:
 
 
 def timestamp():
-    #function to print timstamp for filename
+    # function to print timstamp for filename
     return datetime.now().strftime("%Y%m%d%H%M%S")
 
 
-
-def plot_wait_time_Data(df,figure_dir:Path):
+def plot_wait_time_Data(df, figure_dir: Path):
     # Explode out the wait times and queue counts. for a plot or two.
     df = df.explode(['wait_times', 'cashier_queue', 'usher_queue', 'server_queue'], ignore_index=True)
     # Plot a line.  x axis = employee count y axis = minimum duration.
@@ -176,7 +176,8 @@ def plot_wait_time_Data(df,figure_dir:Path):
     plt.savefig(figure_dir / f"{timestamp()}_wait_time_boxplot.png")
     plt.show()
 
-def plot_customer_count_boxplot(df_serviced, figure_dir:Path):
+
+def plot_customer_count_boxplot(df_serviced, figure_dir: Path):
     plt.figure(figsize=(10, 10))
     # rotate x axis label by 90 degrees
     plt.xticks(rotation=90)
@@ -184,18 +185,20 @@ def plot_customer_count_boxplot(df_serviced, figure_dir:Path):
     plt.savefig(figure_dir / f"{timestamp()}_customer_count_boxplot.png")
     plt.show()
 
+
 def main():
     # Setup
     figure_dir = Path('./plots')
     random.seed(42)
 
     max_employees = 15
-     # create a bunch of theaters
+    # create a bunch of theaters
     # Run the simulation and retrieve the average wait time from each run back into the EmployeeConfig.
-    theaters = [t.run() for t in (Theater(employee_config=ec) for ec in  EmployeeConfig.generate_employee_config(max_employees))]
+    theaters = [t.run() for t in
+                (Theater(employee_config=ec) for ec in EmployeeConfig.generate_employee_config(max_employees))]
 
     df = pd.DataFrame(data=[t.get_result() for t in theaters])
-    df['total_employees'] = df[['num_cashiers','num_ushers', 'num_servers']].sum(axis=1)
+    df['total_employees'] = df[['num_cashiers', 'num_ushers', 'num_servers']].sum(axis=1)
 
     plot_wait_time_Data(df.drop(axis=1, labels=['customer_count']),
                         figure_dir=figure_dir)
@@ -203,7 +206,6 @@ def main():
     # Plot a line.  x axis = employee count y axis = minimum duration.
     plot_customer_count_boxplot(df.drop(axis=1, labels=['wait_times', 'cashier_queue', 'usher_queue', 'server_queue']),
                                 figure_dir=figure_dir)
-
 
 
 if __name__ == "__main__":
